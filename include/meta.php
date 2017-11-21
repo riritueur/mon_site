d953458e02a70126cff8bcfafe22ccae724fb852
Richard Peres <riritueur@gmail.com>
2017-11-21 20:20:50 +0100

    Fin méta

--- include/meta.php
+++ include/meta.php
@@ -6,22 +6,31 @@
 <link rel="shortcut icon" href="ressource/icon/code.png" />
 
 
-<meta name="Auteur" content="Richard Peres" />
+<meta name="author" content="Richard Peres" />
 <meta name="description" content="Bienvenue sur le profil de Richard PERES, étudiant en informatique. Retrouvez en détail mon profil professionnel et n'hésitez pas à me contacter pour toute information." />
 
-
+<meta property="og:type" content="website" />
 <meta property="og:site_name" content="Richard PERES" />
 <meta property="og:url" content="http://richard-peres.xyz" />
 <meta property="og:title" content="Richard PERES" />
-<meta property="og:image" content="ressource/icon/code.svg" />
+<meta property="og:image" content="http://www.richard-peres.xyz/ressource/image/coding.png" />
 <meta property="og:description" content="Bienvenue sur le profil de Richard PERES, étudiant en informatique. Retrouvez en détail mon profil professionnel et n'hésitez pas à me contacter pour toute information." />
+
+<meta name="twitter:image" content="http://www.richard-peres.xyz/ressource/image/coding.png" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:title" content="Richard PERES" />
+<meta name="twitter:description" content="Bienvenue sur le profil de Richard PERES, étudiant en informatique. Retrouvez en détail mon profil professionnel et n'hésitez pas à me contacter pour toute information." />
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-88463922-2"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
   gtag('js', new Date());
 
   gtag('config', 'UA-88463922-2');
+
 </script>
